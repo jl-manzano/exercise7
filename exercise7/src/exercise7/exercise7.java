1c57a2252a2764ab523b30139d653a769d5b30b8
jl.manzano <jl.manzano@106-19.info2>
2024-09-20 14:35:51 +0200

    exercise7 finished

---
+++ exercise7/src/exercise7/exercise7.java
@@ -0,0 +1,14 @@
+package exercise7;
+
+public class exercise7 {
+	public static void main(String[] args) {
+		 System.out.println("    *        *     *");
+         System.out.println("    *        **   **");
+         System.out.println("    *        * * * *");
+         System.out.println("    *        *  *  *");
+         System.out.println("    *        *     *");
+         System.out.println("*   *        *     *");
+         System.out.println("*   *        *     *");
+         System.out.println(" ***         *     *");         
+	}
+}
